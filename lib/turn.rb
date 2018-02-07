76cd10ea2887f292c63beee3fbe51839fcf22bc4
Teresa Terry <dr.terry2013@gmail.com>
2018-02-07 17:41:05 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,7 +12,9 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
+    display_board(board)
   else
+    display_board(board)
     turn(board)
   end
 end
